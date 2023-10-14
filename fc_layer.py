a103b8272703a6a855687f4be2c263204bfea949
bharigopan <46465452+bharigopan@users.noreply.github.com>
2023-10-14 14:25:11 +0530

    Update fc_layer.py

--- fc_layer.py
+++ fc_layer.py
@@ -5,14 +5,17 @@ import numpy as np
 class FCLayer(Layer):
     # input_size = number of input neurons
     # output_size = number of output neurons
+    print("FCLayer", Layer)
     def __init__(self, input_size, output_size):
         self.weights = np.random.rand(input_size, output_size) - 0.5
         self.bias = np.random.rand(1, output_size) - 0.5
+        print("input_size", input_size, "output_size", output_size)
 
     # returns output for a given input
     def forward_propagation(self, input_data):
         self.input = input_data
         self.output = np.dot(self.input, self.weights) + self.bias
+        print("forward_propagation - input_data", input_data)
         return self.output
 
     # computes dE/dW, dE/dB for a given output_error=dE/dY. Returns input_error=dE/dX.
