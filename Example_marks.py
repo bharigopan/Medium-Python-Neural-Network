5fe66765939d58e6cae6e269a781c1a38d98a651
bharigopan <46465452+bharigopan@users.noreply.github.com>
2023-10-14 14:09:32 +0530

    Update Example_marks.py

--- Example_marks.py
+++ Example_marks.py
@@ -3,7 +3,7 @@ import numpy as np
 from network import Network
 from fc_layer import FCLayer
 from activation_layer import ActivationLayer
-from activations import tanh, tanh_prime, sigmoid
+from activations import tanh, tanh_prime, sigmoid, sigmoid_prime
 from losses import mse, mse_prime
 
 # training data
@@ -13,9 +13,9 @@ y_train = np.array([[[82]], [[94]], [[45]], [[42]]])
 # network
 net = Network()
 net.add(FCLayer(3, 2))
-net.add(ActivationLayer(tanh, tanh_prime))
+net.add(ActivationLayer(sigmoid, sigmoid_prime))
 net.add(FCLayer(2, 1))
-net.add(ActivationLayer(tanh, tanh_prime))
+net.add(ActivationLayer(sigmoid, sigmoid_prime))
 
 # train
 net.use(mse, mse_prime)
