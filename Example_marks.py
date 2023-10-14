49d196ee83e92f3b00c3d09e8a54dae554fe99b4
bharigopan <46465452+bharigopan@users.noreply.github.com>
2023-10-14 15:17:43 +0530

    Update Example_marks.py

--- Example_marks.py
+++ Example_marks.py
@@ -13,9 +13,9 @@ y_train = np.array([[[0]], [[1]], [[1]], [[0]]])
 # network
 net = Network()
 net.add(FCLayer(2, 3))
-net.add(ActivationLayer(sigmoid, sigmoid_prime))
+net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(3, 1))
-net.add(ActivationLayer(sigmoid, sigmoid_prime))
+net.add(ActivationLayer(tanh, tanh_prime))
 
 # train
 net.use(mse, mse_prime)
